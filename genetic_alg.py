477e225c75c9851cba2d943110fd71e01a9606ed
Victor Coelho <victorfcguimares@gmail.com>
2024-11-04 13:37:51 -0300

    dados coletados em json

--- genetic_alg.py
+++ genetic_alg.py
@@ -2,30 +2,32 @@ import starter_generator as sg
 from random import randint
 from random import random
 
+
 def print_pop(population, n_pop):
     for i in range(n_pop):
         print(population[i])
     print("\n")
 
+
 def repr(p1, p2, n_itens, m_ratio):
     son = []
-    mid = int(n_itens/2)
-    #crossover
-    for i in range (mid):
+    mid = int(n_itens / 2)
+    # crossover
+    for i in range(mid):
         son.append(p1[i])
-    for i in range (mid, n_itens):
-        son.append (p2[i])
-    
-    #mutação
+    for i in range(mid, n_itens):
+        son.append(p2[i])
+
+    # mutação
     if (random() < m_ratio):
         position = randint(0, (n_itens - 1))
         if son[position] == 1:
             son[position] = 0
         else:
             son[position] = 1
-    
+
     return son
-        
+
 
 def pick_parent(population, sum_eval, n_pop, n_itens):
     aux_rand = randint(0, sum_eval)
@@ -36,26 +38,31 @@ def pick_parent(population, sum_eval, n_pop, n_itens):
             return population.pop(i)
     return population.pop()
 
+
 def new_gen(population, n_pop, sum_eval, n_itens, m_ratio):
     borutos = []
-    for i in range (n_pop):
-        p1 = pick_parent(population, sum_eval, n_pop, n_itens) #remove temporariamente indivíduo
+    for i in range(n_pop):
+        p1 = pick_parent(
+            population, sum_eval, n_pop, n_itens
+        )  # remove temporariamente indivíduo
         p2 = pick_parent(population, (sum_eval - p1[n_itens + 2]), (n_pop - 1), n_itens)
-        population.append(p1)#readicionando os indivíduos na população
+        population.append(p1)  # readicionando os indivíduos na população
         population.append(p2)
-        
+
         borutos.append(repr(p1, p2, n_itens, m_ratio))
-        
+
     return borutos
 
+
 def pick_best(population, n_itens):
-    best_eval = 0
+    best_eval = population[0][n_itens + 2]
     best = population[0]
     for i in population:
-        if (i[n_itens + 2] >= best_eval):
+        if i[n_itens + 2] > best_eval:
             best_eval = i[n_itens + 2]
-            best = (i)
+            best = i
     return best
+
 
 def evaluate(c_max, population, n_itens):
     w_position = n_itens
@@ -63,25 +70,29 @@ def evaluate(c_max, population, n_itens):
     sum_eval = 0
     for i in population:
         eval = i[v_position]
-        if ((i[w_position] - c_max) > 0):
+        if (i[w_position] - c_max) > 0:
+            #se muito pesado penalizar
             eval = int(eval * 0.1)
         sum_eval += eval
-        i.append(eval)   
-    return sum_eval    
-    
+        i.append(eval)
+    return sum_eval
+
+
 def set_value(population, value, n_itens):
     for i in population:
-        v=0
+        v = 0
         for j in range(n_itens):
             v += i[j] * value[j]
         i.append(v)
 
+
 def set_weight(population, weight, n_itens):
     for i in population:
-        w=0
+        w = 0
         for j in range(n_itens):
             w += i[j] * weight[j]
         i.append(w)
+
 
 def init_pop(n_itens, n_pop):
     population = []
@@ -91,24 +102,31 @@ def init_pop(n_itens, n_pop):
         population.append(choice)
     return population
 
+
 def ga_search(n_itens, n_pop, gen_limmit, weight, value, c_max, m_ratio):
     population = init_pop(n_itens, n_pop)
     set_weight(population, weight, n_itens)
     set_value(population, value, n_itens)
     sum_eval = evaluate(c_max, population, n_itens)
-    current_best = pick_best(population, n_itens)
-    print(current_best)
-    
-    for i in range(gen_limmit*2):
+    gen_best = pick_best(population, n_itens)
+    all_time_i = 0 #geração do melhor da história
+    all_time = gen_best #melhor conhecido durante ahistória
+    improv = 0 #numero de melhorias
+
+    for i in range(gen_limmit):
+
         population = new_gen(population, n_pop, sum_eval, n_itens, m_ratio)
         set_weight(population, weight, n_itens)
         set_value(population, value, n_itens)
         sum_eval = evaluate(c_max, population, n_itens)
-        current_best = pick_best(population, n_itens)
-        print(current_best)
-        
-         
-    #print_pop(population, n_pop)
-    #print("Melhor: ")
-    #print(current_best)
-    
+        gen_best = pick_best(population, n_itens)
+        if gen_best[n_itens + 2] > all_time[n_itens + 2]:
+            all_time_i = i + 1
+            all_time = gen_best
+            improv += 1
+    lastgen_best = pick_best(population, n_itens)
+    return all_time[n_itens + 2], all_time_i, lastgen_best[n_itens + 2], improv
+
+    # print_pop(population, n_pop)
+    # print("Melhor: ")
+    # print(current_best)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import starter_generator as sg
 import genetic_alg as ga
 from random import randint
+import json
 
 # cabeçalho, iniciar constantes
 n_itens = 30  # Quantidade de itens do problema
@@ -12,6 +13,26 @@ gen_limmit = 100  # limite de iterações/gerações
 m_ratio = 0.01  # taxa de mutação
 # tempera
 
+def loop_ag(v_size, weight, value, c_max):
+    list_at = []
+    list_last = []
+    list_at_i = []
+    list_improv = []
+    for i in range(v_size):
+        all_time, all_time_i, lastgen_best, improv = ga.ga_search(n_itens, n_pop, gen_limmit, weight, value, c_max, m_ratio)
+        list_at.append(all_time)
+        list_last.append(lastgen_best)
+        list_at_i.append(all_time_i)
+        list_improv.append(improv)
+        print(i)
+    dados = {
+    "list_at": list_at,
+    "list_last": list_last,
+    "list_at_i": list_at_i,
+    "list_improv": list_improv
+    }
+    with open('mut_100perc.json', 'w') as f:
+        json.dump(dados, f, indent=4)
 
 def show_inst(weight, value, c_max):  # dados sobre uma instância do problema
     print("Lista de pesos:\n", weight)
@@ -26,8 +47,7 @@ def main():
         n_itens, w_max, v_max
     )  # inicia itens aleatórios e uma capacidade para a mochila
     show_inst(weight, value, c_max)
-    ga.ga_search(n_itens, n_pop, gen_limmit, weight, value, c_max, m_ratio)
-
+    #loop_ag(100, weight, value, c_max)
 
 if __name__ == "__main__":
     main()
